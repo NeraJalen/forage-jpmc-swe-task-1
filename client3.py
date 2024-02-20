5787bfff94030c7a61e6c79bcb3529cff2583b82
NeraJalen <nerajalen@gmail.com>
2024-02-20 12:24:42 +1100

    Revert "Update client3.py"
    This reverts commit 63a00925188c35ab7146b9aed4db68ae17719653.

--- client3.py
+++ client3.py
@@ -28,26 +28,22 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-# TO BE CHANGED ...........................................................................................
+
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price)/2
+    price = bid_price
     return stock, bid_price, ask_price, price
-#..........................................................................................................
+
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    """ Also create some test units for this function in the client_test.py"""
-    if(price_b == 0):
-        # when price_b is 0 avoid throwing ZeroDivisionError
-        return
-    return price_a/price_b 
-#..........................................................................................................
+    return 1
+
 
 # Main
 if __name__ == "__main__":
@@ -56,11 +52,8 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
-#..........................................................................................................
+        print("Ratio %s" % getRatio(price, price))
